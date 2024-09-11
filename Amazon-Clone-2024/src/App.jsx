5006778fb420f262cf0af3162d2464d3e54cd3b1
Hellen-Shiferaw <yilmahellen@gmail.com>
2024-09-10 21:17:12 -0400

    first commit

---
+++ Amazon-Clone-2024/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Amazon</h1>
+      
+        
+     
+    </>
+  )
+}
+
+export default App
