7ac8ff81b7da365eee50652b3165d00c0e8cc700
Hellen-Shiferaw <yilmahellen@gmail.com>
2024-09-26 23:46:18 -0400

    axios updated

--- Amazon-Clone-2024/src/Api/axios.js
+++ Amazon-Clone-2024/src/Api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-2024-5512b/us-central1/api",
+  // local instence of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-2024-5512b/us-central1/api",
+
+
+  // deployed version of amazon server on render
+baseURL:"https://amazon-api-5r28.onrender.com",
 });
 
 export { axiosInstance };
